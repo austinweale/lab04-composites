4c3f22f38e0521165293d081e20585103912a941
Austin Weale <austinweale@gmail.com>
2017-01-24 13:38:10 -0800

    added method addtext

--- composite-dom.ts
+++ composite-dom.ts
@@ -1,3 +1,5 @@
+import readlineSync = require('readline-sync');
+
 interface IDomElement {
   print();
 }
@@ -5,12 +7,28 @@ interface IDomElement {
 class DomElement implements IDomElement {
 
     private children: DomElement[];
+    private type: string;
 
-    addElement(){
+    constructor(type: string){
+        this.type = type;
+        this.children = [];
+    }
 
+    addText(text: string){
+        this.children.push(new DomElement(text));
+    }
+
+    addElement(other:DomElement){
+        this.children.push(other);
     }
 
     print(){
-
+        var openTag = (this.children.length == 0) ? this.type : "<" + this.type + ">";
+        var closingTag = (this.children.length == 0) ? "" : "</" + this.type + ">";
+        console.log(openTag);
+        for(var i = 0; i < this.children.length; i++){
+            this.children[i].print();
+        }
+        console.log(closingTag);
     }
 }
